5ae83084415edec59f5813e8bcf8f23768e79c5b
Tengfei han <38068286+TengFeiHan0@users.noreply.github.com>
2020-04-24 20:47:19 +0900

    Update run_dso.sh

--- run_dso.sh
+++ run_dso.sh
@@ -1,8 +1,8 @@
   #!/usr/bin/env bash  
  
  ./build/bin/dso_dataset \
-		files=/home/fei/Downloads/sequences/00/image_0 \
-		calib=/home/fei/CNN-DSO/camera.txt \
-		cnn=/home/fei/packnet.pt \
+		files=/path/image_0 \
+		calib=/path/camera.txt \
+		cnn=/path/packnet.pt \
 		preset=0 \
 		mode=1
